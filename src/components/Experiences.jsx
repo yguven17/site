106622e63e1e97bf2d7ef41de279b5f05a790e92
yakupenesguven <yakupenes.guven@gmail.com>
2023-04-15 03:42:51 +0300

    experinces pages added

---
+++ src/components/Experiences.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { selectData } from "../pages/homeSlice";
+import { Element } from "react-scroll";
+// Data
+
+// Components
+import { Col, Container, Row } from "react-bootstrap";
+import { Title } from "./globalStyledComponents";
+
+const StyledExperineces = styled.section`
+  p {
+    font-size: 1.15rem;
+    text-align: left;
+  }
+  pb {
+    font-size: 1.50rem;
+    text-align: left;
+    font-weight: bold;
+    font-family: "Permanent Marker";
+  }
+  pd {
+    font-size: 1.25rem;
+    text-align: center;
+  }
+  .img {
+    width: 18rem;
+    height: 18rem;
+  }
+`;
+
+export default function Experiences() {
+  const { avatar_url} = useSelector(selectData);
+  
+
+  return (
+    <Element name={"Experineces"} id="experineces">
+      <StyledExperineces className="section">
+        <Container>
+          <Container className="d-flex">
+            <Title>
+              <h2>Experiences</h2>
+              <div className="underline"></div>
+            </Title>
+          </Container>
+          <Row className="align-items-center mt-5">
+            <Col className="d-flex flex-column text-center">
+              <Container>
+
+     
+<pb  >Siemens, Part-time Working Student</pb>
+<br /><br />
+<pd  >02/2023 &ndash; present İstanbul, T&uuml;rkiye</pd>
+<p  >&bull;DevOps<br />&bull;Configuration Management<br />&bull;Installing virtual machines and setting up Jenkins, Docker tasks</p>
+<pb  >Schneider Electric, System Software Architecture Engineer Part Timer</pb>
+<br /><br />
+<pd >01/2022 &ndash; 01/2023 İstanbul, T&uuml;rkiye</pd>
+<p  >&bull;System Architecture (AVEVA software's, project interfaces and necessary data and simulations for the project) (4 months)<br />&bull;IT (error analysis and reporting systems and application development, Project construction with Python, working with shell) (1 month)<br />&bull;Industry Sales Team (sales of products analysis and systems to companies, Python coding to streamline sales processes) (1 month)<br />&bull;PLC and SCADA Systems (system examination, software and setup) (4 months)</p>
+<pb  >Siemens, Intern</pb>
+<br /><br />
+<pd  >06/2021 &ndash; 07/2021 İstanbul, T&uuml;rkiye</pd>
+<p  >&bull;PLC and SCADA (to examine automation systems and monitor errors in the system)</p>
+                
+              </Container>
+            </Col>
+            <Col className="d-none d-md-block text-center">
+              <img
+                src={avatar_url}
+                alt="GitHub Avatar"
+                loading="lazy"
+                className="mx-auto rounded-circle"
+                style={{ width: "15rem", height: "15rem" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </StyledExperineces>
+    </Element>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { selectData } from "../pages/homeSlice";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
+import EXperiences from "../components/Experiences";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
@@ -30,6 +31,7 @@ export default function Home() {
       </Element>
       <main>
         <AboutMe />
+        <EXperiences />
         <Skills />
         <Projects />
         <Contact />
